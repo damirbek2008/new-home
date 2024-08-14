85baeb997dcf15b1045175109cc2aec8095dbe2e
damirbek2008 <damirbekkamoliddinov2008@gmail.com>
2024-08-14 14:40:34 +0500

    this is the very first comment

---
+++ index.js
@@ -0,0 +1,258 @@
+let numbers = [1, 2, 4, 5,6,7 , 8, 12, 14,]
+let numbers1 =[21,341,64,865,6782465,356,82,56,856812,456,45,-346,-3426,4,-578,-58,-45,-36,-34]
+
+// First challenge
+// let check = (check) => check >=0;
+
+// let check= function (check ) {
+//     chek >=0
+// } 
+// console.log(numbers.filter(check))
+
+
+
+
+// Second challenge
+// let check1=(check) => check>0
+// console.log(numbers.every(check1))
+
+
+
+
+
+
+// Third challenge
+// console.log(numbers.concat(numbers1))
+
+
+
+
+
+
+
+// 4th challenge
+// console.log(numbers.slice(2 ,10 ), numbers1.slice(3,10))
+
+
+
+
+
+
+
+// // 5th challenge
+// console.log(numbers1.sort((a, b) => a-b ));
+
+
+
+
+
+
+
+ // 6th challenge
+// console.log(numbers.reverse());
+// console.log(numbers1.reverse());
+
+
+
+
+
+
+
+
+
+ // 7th challenge
+// console.log(numbers.join(',,'));
+
+
+
+
+
+
+
+
+
+
+ // 1st homework
+
+// function showDb (){
+//     let noth;
+//     let privite = {
+//         age: 21,
+//         name: 'Damirwbwk',
+//         surname:' Kamoliddinov',
+//     }
+//     // privite=true;
+//     // console.log(privite);
+
+
+//     if (privite){
+//         console.log(privite)
+//     }else{
+//         console.log(noth);
+//     }
+
+// }
+
+// showDb()
+
+
+
+
+
+
+
+
+//  // 2st homework
+// function writeGenres () {
+   
+//  for(let i = 1; i < 4; i++) {
+        
+//     let chart= prompt(`Yaxshi ko'rgan kinongiz ${i}`)
+//    console.log(chart)
+//    let massiv =[];
+//    console.log(massiv.push(chart));
+//     }
+  
+// }
+
+// writeGenres()
+
+
+
+
+
+
+
+
+
+
+// const fullname = "Damirbek Kamolliddinov"
+
+// // let lastchar=fullname.slice(-1);
+
+
+
+// // let firstchar =fullname.slice(0, fullname.indexOf(" "))
+// // let lastname =fullname.slice(fullname.indexOf(" ")+1 )
+
+// console.log(firstchar)
+// console.log(lastname)
+
+
+
+
+
+// const email ="damirbekkamoliddinov@gmail.com"
+
+
+// let username=email.slice(0,email.indexOf("@"))
+// let extenction=email.slice(email.indexOf("@")+1)
+
+// console.log(username);
+// console.log(extenction);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method chaining = Calling one method after another
+//                   in one coonsition line of code
+
+
+// let username = window.prompt("Enter your userame")
+
+// -----------------No Method Chaining------
+
+
+
+
+
+// username = username.trim();
+// let letter = username.charAt(0);
+// letter=letter.toUpperCase();
+
+// let extraChart = username.slice(1)
+// extraChart=extraChart.toLocaleLowerCase();
+
+
+// username = letter + extraChart
+
+
+// console.log(username);
+
+
+
+
+
+
+// -------------------Method Chaining------
+
+
+
+
+// username = username.trim().charAt(0).toUpperCase() + username.slice(0).toLocaleLowerCase();
+
+// console.log(username);
+
+
+
+
+
+
+// Logical operators = used to combine or manipulate boolean values 
+//                            (true or false)
+
+
+
+
+                                //  AND=&&
+                                //  OR=||
+                                //  Not=!
+
+
+
+
+// const temp = 200;
+
+
+// if(temp > 0 && temp <= 30  ){
+//     console.log("The weather is good");
+
+// }else{
+//     console.log("The weather is really bad");
+// }
+
+
+
+// const isSunny =false;
+
+// if (!isSunny){
+//     console.log("It is SUNNY")
+// }else{
+//     console.log("It is cloudy")
+// }
+
+
+
+
+
+
+    
